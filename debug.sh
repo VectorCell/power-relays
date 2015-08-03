ba61c50628dd4bd303ed7a335934d2bfe8c7ada3
Brandon Smith <vectorcell@gmail.com>
2015-08-03 04:53:03 -0500

    allows passing arguments through debug script

--- debug.sh
+++ debug.sh
@@ -2,6 +2,7 @@
 
 # for testing on a machine without wiringPi.h
 
+rm -f main-debug
 g++ -pedantic -std=c++0x -Wall -MD -c main.c++ -DDEBUG
 g++ -pedantic -std=c++0x -Wall main.o -o main-debug -DDEBUG
-./main-debug
+./main-debug $@
