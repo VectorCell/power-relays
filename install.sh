50a918f35c09f484f3bbb14534ca387082c9e621
Brandon Smith <vectorcell@gmail.com>
2017-02-18 22:26:53 -0600

    changes to reflect hardware migration

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ cd ~/bin
 ln -s ~/Programs/power-relays/relays
 
 cd -
-if [ -e /var/www/gui ]; then
-	sudo rm -rf /var/www/gui
+if [ -e /var/www/html/gui ]; then
+	sudo rm -rf /var/www/html/gui
 fi
-sudo rsync -r --progress gui /var/www/
-sudo cp pins.conf /var/www/gui/
+sudo rsync -r --progress gui /var/www/html/
+sudo cp pins.conf /var/www/html/gui/
 
 sudo service apache2 restart
